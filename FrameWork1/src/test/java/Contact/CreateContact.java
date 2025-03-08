1cee6265c44b083792215885ad04ae11b2e1ab01
Admin <Admin@DESKTOP-4SV1OS7>
2025-03-08 11:38:36 +0530

    FramWork1

---
+++ FrameWork1/src/test/java/Contact/CreateContact.java
@@ -0,0 +1,14 @@
+package Contact;
+
+import org.testng.annotations.Test;
+
+public class CreateContact {
+@Test
+public void AccountCreate() {
+	System.out.println("Account created");
+}
+@Test
+public void login() {
+	System.out.println("loginDone");
+}
+}
---
+++ FrameWork1/src/test/java/Oganization/CreateORG.java
@@ -0,0 +1,16 @@
+package Oganization;
+
+import org.testng.annotations.Test;
+
+public class CreateORG {
+	@Test
+	public void CreateOrganization() {
+		System.out.println("ORg created");
+		
+	}
+	@Test
+	public void logout() {
+		System.out.println("logedout successfully");
+	}
+
+}
